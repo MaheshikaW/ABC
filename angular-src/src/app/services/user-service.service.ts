f468626b97a953d9a5ac140a7fb7cc8840b130aa
Maheshika Weerasinghe <maheshiweera11@gmail.com>
2018-12-27 13:53:48 +0530

    final

--- angular-src/src/app/services/user-service.service.ts
+++ angular-src/src/app/services/user-service.service.ts
@@ -1,28 +1,46 @@
-import { Injectable } from '@angular/core'; {}
-import { HttpClient } from "@angular/common/http";
+import { Injectable } from '@angular/core';
 import {Http,Headers} from '@angular/http';
 import {map} from 'rxjs/operators';
-import axios from 'axios';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserServiceService {
+export class StudentserviceService {
 
-  constructor(private httpclient:HttpClient,private http:Http ) { }
+  constructor(
+    private http:Http
+  ) { }
 
-addStudent(student){
-  console.log(student);
-  let headers = new Headers();
+  insertStudent(student){
+    let headers = new Headers();
     headers.append('Content-Type', 'application/json');
     return this.http.post('http://localhost:3000/student/newstudent',student,{headers:headers})
       .pipe(map(res => res.json()));
+  
+  }
 
-}
+  deleteStudent(id) {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.delete('http://localhost:3000/student/'+id,{headers:headers})
+      .pipe(map(res => res.json()));
+  }
+
+  getOneStudent(id){
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.get('http://localhost:3000/student/getstudent/'+id,{headers:headers})
+      .pipe(map(res => res.json()));
+  }
 
 
+  editStudent(id,student) {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post('http://localhost:3000/student/editstudent'+id,student,{headers:headers})
+      .pipe(map(res => res.json()));
 
-
+  }
 }
 
 
